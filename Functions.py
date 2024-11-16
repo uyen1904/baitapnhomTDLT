be160c26c81e6a8d7a7548d1353673bc31a51168
ainhing <galalania1@gmail.com>
2024-11-17 00:10:38 +0700

    minhphuong

--- Functions.py
+++ Functions.py
@@ -1,14 +1,10 @@
-def solve_equation(a,b,result):
-            if a == 0:
-                if b == 0:
-                    result = "Infinite solutions (any x satisfies the equation)"
-                else:
-                    result = "No solution (inconsistent equation)"
-            else:
-                x = -b / a
-                result = f"The solution is: x = {x}"
-            return result
-
-
-
-
+def solve_equation(a, b):
+    if a == 0:
+        if b == 0:
+            result = "Infinite solutions (any x satisfies the equation)"
+        else:
+            result = "No solution (inconsistent equation)"
+    else:
+        x = -b / a
+        result = f"The solution is: x = {x}"
+    return result
